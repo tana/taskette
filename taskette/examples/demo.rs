d7c1b3933f9bc54689cb9ec163496a69ae458128
Satoshi Tanaka <overthere555@gmail.com>
2025-11-15 04:07:45 +0900

    Context switching (still buggy)

--- taskette/examples/demo.rs
+++ taskette/examples/demo.rs
@@ -3,7 +3,11 @@
 
 use log::info;
 use panic_semihosting as _;
-use taskette::{Kernel, KernelConfig};
+use static_cell::StaticCell;
+use taskette::{Kernel, KernelConfig, Stack};
+
+static TASK1_STACK: StaticCell<Stack<8192>> = StaticCell::new();
+static TASK2_STACK: StaticCell<Stack<8192>> = StaticCell::new();
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -22,10 +26,28 @@ fn main() -> ! {
     let peripherals = cortex_m::Peripherals::take().unwrap();
     let mut kernel = Kernel::new(
         peripherals.SYST,
+        peripherals.SCB,
         12_000_000,
-        KernelConfig::default().with_tick_freq(1),
+        KernelConfig::default().with_tick_freq(10),
     );
-    kernel.start();
 
-    loop {}
+    let task1_stack = TASK1_STACK.init(Stack::new());
+    let _task1 = kernel.spawn(task1_stack, || {
+        let mut i = 0;
+        loop {
+            log::info!("task1 {}", i);
+            i = (i + 1) % 10000;
+        }
+    }).unwrap();
+
+    let task2_stack = TASK2_STACK.init(Stack::new());
+    let _task2 = kernel.spawn(task2_stack, || {
+        let mut i = 0;
+        loop {
+            log::info!("task2 {}", i);
+            i = (i + 1) % 10000;
+        }
+    }).unwrap();
+
+    kernel.start();
 }
--- taskette/src/lib.rs
+++ taskette/src/lib.rs
@@ -1,7 +1,25 @@
 #![no_std]
 
-use cortex_m::peripheral::{SCB, syst::SystClkSource};
-use log::{info, trace};
+use core::cell::RefCell;
+
+use cortex_m::{
+    peripheral::{SCB, scb::SystemHandler, syst::SystClkSource},
+    register::control::Spsel,
+};
+use critical_section::Mutex;
+use heapless::Vec;
+use log::{debug, info, trace};
+
+const MAX_NUM_TASKS: usize = 10;
+
+static STACK_POINTERS: Mutex<RefCell<Vec<usize, MAX_NUM_TASKS>>> =
+    Mutex::new(RefCell::new(Vec::new()));
+static CURRENT_TASK: Mutex<RefCell<usize>> = Mutex::new(RefCell::new(0));
+
+#[derive(Clone, Debug)]
+pub enum Error {
+    TaskFull,
+}
 
 #[non_exhaustive]
 pub struct KernelConfig {
@@ -22,20 +40,57 @@ impl Default for KernelConfig {
 
 pub struct Kernel {
     syst: cortex_m::peripheral::SYST,
+    scb: cortex_m::peripheral::SCB,
     clock_freq: u32,
     config: KernelConfig,
 }
 
 impl Kernel {
-    pub fn new(syst: cortex_m::peripheral::SYST, clock_freq: u32, config: KernelConfig) -> Self {
+    pub fn new(
+        syst: cortex_m::peripheral::SYST,
+        scb: cortex_m::peripheral::SCB,
+        clock_freq: u32,
+        config: KernelConfig,
+    ) -> Self {
+        // Reserve Task #0 for idle task
+        critical_section::with(|cs| {
+            let mut stack_pointers = STACK_POINTERS.borrow_ref_mut(cs);
+            stack_pointers.push(0).unwrap();
+        });
+
         Kernel {
             syst,
+            scb,
             clock_freq,
             config,
         }
     }
 
-    pub fn start(&mut self) {
+    pub fn start(&mut self) -> ! {
+        critical_section::with(|_| unsafe {
+            // Copy the value of Main (current) Stack Pointer to the the Process Stack Pointer
+            cortex_m::register::psp::write(cortex_m::register::msp::read());
+
+            // Change the stack to the Process Stack (PSP)
+            let mut control = cortex_m::register::control::read();
+            control.set_spsel(Spsel::Psp);
+            cortex_m::register::control::write(control);
+        });
+
+        // On armv6m `set_priority` is not atomic
+        critical_section::with(|_| unsafe {
+            // Set priorities of core exceptions
+            self.scb.set_priority(
+                SystemHandler::PendSV,
+                255, /* Lowest possible priority */
+            );
+            self.scb.set_priority(
+                SystemHandler::SysTick,
+                255, /* Lowest possible priority */
+            );
+        });
+
+        // Configure the SysTick timer
         self.syst.set_clock_source(SystClkSource::Core);
         self.syst
             .set_reload(self.clock_freq / self.config.tick_freq);
@@ -43,6 +98,41 @@ impl Kernel {
         self.syst.enable_counter();
 
         info!("Kernel started");
+
+        loop {
+            trace!("Idle");
+            cortex_m::asm::wfi();
+        }
+    }
+
+    pub fn spawn<const N: usize>(
+        &self,
+        stack: &mut Stack<N>,
+        func: fn(),
+    ) -> Result<TaskHandle, Error> {
+        // Prepare initial stack of the task
+        let initial_sp = unsafe {
+            let sp = stack.0.as_mut_ptr_range().end;
+            let sp = push_to_stack(sp, HardwareSavedRegisters::from_pc(func as u32));
+            let sp = push_to_stack(sp, SoftwareSavedRegisters::new());
+            sp
+        };
+
+        let Ok(task_id) = critical_section::with(|cs| {
+            let mut stack_pointers = STACK_POINTERS.borrow_ref_mut(cs);
+            stack_pointers.push(initial_sp as usize)?;
+
+            Ok::<usize, usize>(stack_pointers.len() - 1)
+        }) else {
+            return Err(Error::TaskFull)
+        };
+
+        info!("Task #{} created", task_id);
+        debug!("Stack from={:08X} to={:08X}", stack.0.as_ptr_range().start as usize, stack.0.as_ptr_range().end as usize);
+
+        Ok(TaskHandle {
+            id: task_id,
+        })
     }
 }
 
@@ -50,20 +140,35 @@ impl Kernel {
 #[unsafe(no_mangle)]
 #[unsafe(naked)]
 extern "C" fn PendSV() {
+    // Registers {R0-R3, R12, LR, PC, xPSR} are saved in the process stack by the hardware
     core::arch::naked_asm!(
-        "mov r0, sp",   // Use the SP value as the argument for `switch_context`
-        "push {{lr}}",   // Save LR that is modified by the next BL
-        "bl {switch_context}",  // Call `switch_context` function
-        "pop {{lr}}",    // Restore LR (to EXC_RETURN)
-        "mov sp, r0",   // Change SP into the value returned by `switch_context`
+        "mrs r0, psp",  // Read the process stack pointer (PSP, because the SP is MSP now)
+        "stmfd r0!, {{r4-r11}}", // Save the remaining registers in the process stack
+        "push {{lr}}",   // Save LR (that is modified by the next BL) in the main stack
+        "bl {switch_context}",  // Call `switch_context` function. R0 (process stack pointer) is used as the first argument and the return value.
+        "pop {{lr}}",    // Restore LR (to EXC_RETURN) from the main stack
+        "ldmia r0!, {{r4-r11}}",  // Restore the registers not saved by the hardware from the process stack
+        "msr psp, r0",   // Change PSP into the value returned by `switch_context`
         "bx lr",
         switch_context = sym switch_context,
     );
+    // Hardware restores registers R0-R3 and R12 from the new stack
 }
 
 extern "C" fn switch_context(orig_sp: usize) -> usize {
-    trace!("orig_sp = {:08X}", orig_sp);
-    orig_sp
+    critical_section::with(|cs| {
+        let current_task = CURRENT_TASK.borrow_ref(cs);
+        STACK_POINTERS.borrow_ref_mut(cs)[*current_task] = orig_sp;
+    });
+    let next_sp = critical_section::with(|cs| {
+        let stack_pointers = STACK_POINTERS.borrow_ref(cs);
+        let mut current_task = CURRENT_TASK.borrow_ref_mut(cs);
+        *current_task = (*current_task + 1) % stack_pointers.len();
+
+        stack_pointers[*current_task]
+    });
+    trace!("Context switch: orig_sp = {:08X}, next_sp = {:08X}", orig_sp, next_sp);
+    next_sp
 }
 
 #[cortex_m_rt::exception]
@@ -72,6 +177,94 @@ fn SysTick() {
     yield_now();
 }
 
+unsafe fn push_to_stack<T>(sp: *mut u8, obj: T) -> *mut u8 {
+    unsafe {
+        let sp = sp.byte_sub(size_of::<T>());
+        *(sp as *mut T) = obj;
+
+        sp
+    }
+}
+
 pub fn yield_now() {
     SCB::set_pendsv();
+}
+
+/// Correctly aligned stack
+/// Reference: https://docs.rs/rp2040-hal/0.11.0/rp2040_hal/multicore/struct.Stack.html
+#[repr(align(8))]
+pub struct Stack<const N: usize>([u8; N]);
+
+impl<const N: usize> Stack<N> {
+    pub fn new() -> Self {
+        Self([0u8; N])
+    }
+}
+
+#[derive(Debug)]
+pub struct TaskHandle {
+    id: usize,
+}
+
+impl TaskHandle {
+    pub fn id(&self) -> usize {
+        self.id
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Debug)]
+struct HardwareSavedRegisters {
+    r0: u32,
+    r1: u32,
+    r2: u32,
+    r3: u32,
+    r12: u32,
+    lr: u32,
+    pc: u32,
+    xpsr: u32,
+}
+
+impl HardwareSavedRegisters {
+    fn from_pc(pc: u32) -> Self {
+        Self {
+            r0: 0,
+            r1: 0,
+            r2: 0,
+            r3: 0,
+            r12: 0,
+            lr: 0,
+            pc,
+            xpsr: 1 << 24,  // Thumb state
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Debug)]
+struct SoftwareSavedRegisters {
+    // Software-saved registers
+    r4: u32,
+    r5: u32,
+    r6: u32,
+    r7: u32,
+    r8: u32,
+    r9: u32,
+    r10: u32,
+    r11: u32,
+}
+
+impl SoftwareSavedRegisters {
+    fn new() -> Self {
+        Self {
+            r4: 0,
+            r5: 0,
+            r6: 0,
+            r7: 0,
+            r8: 0,
+            r9: 0,
+            r10: 0,
+            r11: 0,
+        }
+    }
 }
